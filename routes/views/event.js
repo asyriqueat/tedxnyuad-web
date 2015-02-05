10e25dd7d30ccfaf9b409d49766e2e40f0db81dd
Frederik Jensen <brinck10@gmail.com>
2015-02-05 21:55:02 +0400

    Text corrections.

--- routes/views/event.js
+++ routes/views/event.js
@@ -12,14 +12,14 @@ exports = module.exports = function(req, res) {
     
     locals.content = [{ template:'focus',
                         macro:'standard',
-                        arguments:{ text:'<p>At TEDxNYUAD we believe in opening new horizons and broadening our perspectives. As we dream to inspire people to make an impact, this year we have assembled a host of speakers who question how we view the world. We want to take our audience on a journey of the unconventional.</p><p>For TEDxNYUAD 2015 we will be exploring the theme <span style="color:red">Question.</span> Our speakers will be sharing their untold stories as they redefine the concepts of home and memory, provide a fresh look on traditions and videogaming, change the way we see – or don’t see – chess, and show the true power of youth.',
+                        arguments:{ text:'<p>At TEDxNYUAD we believe in seeking new horizons and broadening  perspectives. As we dream to inspire people to make an impact, this year we assembled a host of speakers who question how we view the world. We want to take our audience on a journey of the unconventional.</p><p>For TEDxNYUAD 2015 we explore the theme <span style="color:red">Question.</span> Our speakers will redefine the concepts of home and memory, provide a fresh look on traditions and videogaming, change the way we see – or don’t see – chess, and show the true power of youth.',
                                     theme:'Question.'
                         }
                       },
                       { template:'panorama',
                         macro:'standard',
                         arguments:{src:'venue.jpg',
-                                   text:'<h4>Venue</h4>The 2015 TEDxNYUAD will take place in the Conference Center, located in the A6 building on the east side of NYUAD\'s Saadiyat Island campus.  The Conference Center contains black leather chairs, ample lighting, and excellent acoustics, ensuring that those attending will have a comfortable and educational experience.'
+                                   text:'<h4>Venue</h4>The 2015 TEDxNYUAD will take place in the New York University Abu Dhabi Conference Center, located in the A6 building on the east side of our campus on Saadiyat Island. The Conference Center contains black leather chairs, ample lighting, and excellent acoustics, ensuring that those attending will have a comfortable and educational experience.'
                                   }
                       },
                       { template:'standard',
