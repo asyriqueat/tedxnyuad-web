4eb0d895ba33f3cb884088827ad253c6b94a908a
Asyrique AT <asyrique@gmail.com>
2015-02-09 08:47:05 +0400

    Added favicon and map and Partners section (revisit code in future to clean up)

--- public/js/data/event.js
+++ public/js/data/event.js
@@ -12,10 +12,27 @@ exports.content = module.exports = {content:
                                   }
                       },
                       { template:'standard',
-                        macro:'image',
-                        arguments:{src:'images/location.jpg',
+                        macro:'maps',
+                        arguments:{map:{"lat":24.5227104,"long":54.434284},
+                                   apiKey:process.env.GOOGLE_API_KEY,
                                    title:'Address',
                                    text:'<p>The address of the campus at which the TEDxNYUAD will be held is: </p><ul><li><p>New York University Abu Dhabi (NYUAD)</p></li><li><p>Saadiyat Island</p></li><li><p>P.O. Box 129188</p></li><li><p>Abu Dhabi, UAE</p></li></ul><p>See the map for directions. Upon arrival look follow the signs to the Conference Center. In case you come by car, make sure to get a parking ticket.</p>'
                                   }
-                      } 
+                      },
+                      {
+                        template:'standard',
+                        macro: 'logodisplay',
+                        arguments:{arrangement:[],
+                                   title:'Thank you!',
+                                   text: '<p>We would not be able to pull off this incredible event without the help and generous support of our wonderful partners:</p>',
+                                   partners:[{
+                                              name: "NYUAD Institute",
+                                              logo:"./images/partners/nyuad-institute.png",
+                                             },
+                                             {
+                                              name: "NYUAD Office of Student Activities",
+                                              logo: "./images/partners/nyuad-office-student-activities.png"
+                                             }]
+                                  }
+                      }
                      ]};
