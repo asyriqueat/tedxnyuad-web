3038851b473442b0622bf521d1284bd18c66927e
Frederik Jensen <brinck10@gmail.com>
2015-02-10 20:10:27 +0400

    More fixes.

--- public/js/data/index.js
+++ public/js/data/index.js
@@ -5,7 +5,7 @@ exports = module.exports = {content:
                       [{template:'panorama',
                         macro:'standard',
                         arguments:{ right:'15px',
-                                    text:'<h4>Charlotte Bekker</h4>With the help of IKEA furniture, Dutch flea markets, and the image of Dubai, I take a closer look at the irony of constructing authenticity.',
+                                    text:'<h4>Charlotte de Bekker</h4>With the help of IKEA furniture, Dutch flea markets, and the image of Dubai, I take a closer look at the irony of constructing authenticity.',
                                     src:'speakers/Charlotte.jpg'}
                       },
                       { template:'blog',
