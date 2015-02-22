d65ea0fa8ab7a0daec9e2f57e01ddcb50905813a
Frederik Jensen <brinck10@gmail.com>
2015-02-22 22:52:26 +0400

    Featured Mohit – changed text.

--- public/js/data/index.js
+++ public/js/data/index.js
@@ -5,7 +5,7 @@ exports = module.exports = {content:
                       [{template:'panorama',
                         macro:'standard',
                         arguments:{ right:'15px',
-                                    text:'<h4>Mohit Mandal</h4>I played street-cricket with a group of "migrant workers" in an abandoned car park in Abu Dhabi, and learned to challenge my assumptions',
+                                    text:'<h4>Mohit Mandal</h4>Having experienced the multiculturalism of Abu Dhabi, I will share my thoughts on a conundrum that continually confronts me: How does one begin to capture the rich, complex - and, often, invisible - lives of the "migrant workers" in a way that is neither condescending nor patronizing?',
                                     src:'speakers/Mohit.jpg'}
                       },
                       { template:'blog',
