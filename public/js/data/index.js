8e11d6a6a93341bcdf90457a615f364b56d86eca
Frederik Jensen <brinck10@gmail.com>
2015-05-23 14:27:12 +0200

    Front image attached and responsiveness fix implemented.

--- public/js/data/index.js
+++ public/js/data/index.js
@@ -4,9 +4,7 @@ exports = module.exports = {content:
                             
                       [{template:'panorama',
                         macro:'standard',
-                        arguments:{ boxes:[{right:'15px',
-                                    text:'<h4>The Speakers Team</h4>Thanks for a great TEDxNYUAD2015! We hope that you enjoyed the talks. The videos will be up shortly.'}],
-                                    src:'speakers.jpg'}
+                        arguments:{src:'splash.jpg'}
                       },
                       { template:'blog',
                         macro:'standard',
--- public/js/data/talks.js
+++ public/js/data/talks.js
@@ -1,77 +1,77 @@
 exports.content = module.exports = {content:
                      [{ template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>Amer led one of Brazil’s largest peaceful campaigns against corruption in 2012. With the support of over 25,000 students, Amer and his team changed laws in Brazil to promote political transparency.</p><p>Having experienced one of the effects of corruption and inequality, Amer offers insights on how students might feel their efforts to make a difference in the world are worthless, and how they believe their immediate actions for progress might not have any long-term effects. The goal of this talk is to demonstrate to students in a larger community that they have a voice; their goal should be to make their voices louder.</p>',
                                     title:'Amer Nasr – Ordem e Progresso: Student Voices and the Politics of Sweeping Corruption',
                                     src:'https://www.youtube.com/embed/D-brTiSwyLk'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>Attilio is a Chilean actor, theater-maker and avid video gamer. By combining his passion and love for theater and video games, Attilio will take us on an exciting journey of finding the heroes in ourselves.</p><p> Since he can remember, Attilio Rigotti has been obsessed with video games. But it took directing an interactive theater performance to help him figure out why. Simply put, playing video games makes him feel happier. Reflecting on the idea of choice and being a hero in one’s own life, as well as the possibilities of interactivity in the arts, Attilio hopes to better understand what he considers the most important art form of the 21st century and how it can change people\'s lives in the way video games changed his.</p>',
                                     title:'Attilio Rigotti – How video games saved my life',
                                     src:'https://www.youtube.com/embed/PKyOKvKNIU8'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p> Charlotte is an artist and filmmaker who grew up in the Emirate of Ras Al Khaimah. Her art has been featured in a gallery in Dubai, and her latest film Oma en Opa premiered at the Big Sky Documentary Film Festival in Missoula, Montana.</p><p>Somewhere around the age of 17, in the midst of her hipster angst, lies the moment when Charlotte became hyperaware of all that consumerism entails. Blaming the UAE for these troubles, her realization was soon accompanied by an obsessive desire to find "true authenticity" among the skyscrapers, and bling of the young country she was living in. With the help of IKEA furniture, Dutch flea markets, and the image of Dubai, Charlotte will attempt to convey all that her search led her to realize.</p>',
                                     title:'Charlotte de Bekker – AUTHENTÏK: IKEA furniture and the construction of authenticity',
                                     src:'https://www.youtube.com/embed/0kHx4g_d0iE'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>Hayat, from Ethiopia, is the co-founder of PenAfrica, an organization that seeks to promote African literature, connect young writers, and deconstruct the negative image of Africa.</p><p>Stories can be a manifestation of our identities; they allow us to express ourselves. However, it is the case that some stories are told far more often than others and gradually transform to single narratives and stereotypes. Learning and building common understanding is a gradual and continuous process accomplished through sharing and listening. So the question is, how do we make sure that our voices are heard and that our stories are told the right way?</p>',
                                     title:'Hayat Muhammed Seid – Telling stories and claiming self-identities',
                                     src:'https://www.youtube.com/embed/ez90KKDikNU'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>At the age of 5, Jovan became the youngest competitor at the Montenegro National Youth Chess Competition. But simply playing chess was not enough; he had to do it blindfolded. He will be talking about his journey thereafter.</p><p>Training your eyes to see a chessboard that isn’t there and move the game\'s 32 pieces across that very same 64-square matrix; that is the game of blindfolded chess. It is a unique challenge that shows what remarkable potential our brains have, a form of training in which everyone can and should engage, regardless of interest in chess. Allow Jovan to patiently guide you through this game, showing you the many ways in which you can benefit from it on a daily basis.</p>',
                                     title:'Jovan Jovancevic – When vision triumphs over sight',
                                     src:'https://www.youtube.com/embed/nTilwLpf5pc'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>As a young Emirati woman wearing the Abaya, Maitha is exposed to continuous questions regarding her choice of dress.</p><p>When Maitha started university she was bombarded with absurd questions about her abaya that never occurred to her before. These questions have made her realize that the way she feels about and perceives her abaya is different to how others do. Through her talk she aims to explain and dismiss the stereotype of abayas in the context of the UAE. Moreover, she aims to motivate people to ask questions so as to break the stereotypes that surround them.</p>',
                                     title:'Maitha Al Memari – De-stereotyping the \'Black Cloak\'',
                                     src:'https://www.youtube.com/embed/dhAigAI684s'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>In high school, Meera was a bully. After having studied Psychology at Zayed University in Dubai, she is today an advocate of social issues in the UAE. Her passion is to help young bullies overcome their own evils, and change how schools deal with bullying.</p><p> Should bullies be punished or rewarded? It seems that there is an obvious answer to this question but you’d be surprised to know that there isn’t. Meera is going to share her story as a bully and introduce new ideas and interventions for bullies. You can’t fix a car unless you know how the mechanics work, right? Similarly you can’t fix bullies unless you know how a bully’s mind works. Meera was once a bully, and she can tell you exactly how it does.</p>',
                                     title:'Meera Al Agroobi – Bullies are victims too!',
                                     src:'https://www.youtube.com/embed/taEoDwM0Z2o'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>Mohit is an Indian writer and storyteller who has lived in Abu Dhabi for the past four years. Through his interactions with the immigrant community in the city, Mohit will recount the many stories he\'s stumbled upon... while playing street cricket.</p><p> A research assignment and a chance encounter in a barbershop provided Mohit with the spark for a 14-week long journey into the hidden world of Abu Dhabi\'s "migrant workers”. He found himself confronted, continually, with the conundrum: How does one begin to capture the rich, complex - and, often invisible - lives of the "migrant workers" in a way that is neither condescending nor patronizing?</p>',
                                     title:'Mohit Mandel – What cricket taught me about compassion',
                                     src:'https://www.youtube.com/embed/33c1bLFtHm0'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>Samuel, an avid traveller with a passion for science and film, will be questioning the idea of ‘home’ and what this means to him.</p><p>Who is home for you? What is home for you? Where is home for you? Sam used to have only one home – it was the place where he grew up and lived with his family. He felt very comfortable there. Fast-forward through three years of college in Abu Dhabi and Sam now finds himself questioning what home is and how to find it. Sam wants to explore the idea that a person can have multiple homes, the ways a place can become home and the importance of developing memories where you are in life.</p>',
                                     title:'Samuel Ridgeway – Let me take you home...',
                                     src:'https://www.youtube.com/embed/zkI6Jy2Ugck'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>A female Qatari social entrepreneur, Sara created the “Behind the Glamour” project, which changed the laws to secure primary education for the children of migrant workers in Doha.</p><p>How the words of a mother led to a food donation drive, how the tug of an abaya became an educational support program, how a game of football in Mozambique created an overseas call to action, how the desire for a family portrait built a roof and a home. This is a story of how curiosity and unlikely interactions led her to reevaluate her life and find a passion worth pursuing.</p>',
                                     title:'Sara Al Shamlan – Curiosity Saved the Cat',
                                     src:'https://www.youtube.com/embed/AQSsxoznETU'
                         }
                       },
                       { template:'standard',
                         macro:'video',
-                        arguments:{ text:'',
+                        arguments:{ text:'<p>Vasily is an engineering student and technology enthusiast. He and his colleagues are the recipients of the UAE Drones for Good Award 2015, which was awarded AED 1 million for project implementation.</p><p>Engineers are known for creating cool new technology without a solvable problem behind it to justify its existence. Vasily is working on a team of NYUAD engineers, who are solving an existing problem in a national park in the UAE through a human-centered approach, rather than by means of a strictly technological mindset.</p>',
                                     title:'Vasily Rudchenko – What happens when engineers stop caring about technology?',
                                     src:'https://www.youtube.com/embed/EY0GjsRGZ7s'
                         }
