32afb8ba46e0ed700b430704bb5b981a57c08283
ShujaatMirza <msm622@nyu.edu>
2016-03-30 01:27:07 +0400

    Updated Speaker Selection, Speaker's data and Team's bios

--- public/js/data/speakers.js
+++ public/js/data/speakers.js
@@ -1,103 +1,77 @@
 var speakersArray =  [{
-                  name:'Amer Nasr',
+                  name:'Krishna Gaire',
                   id:1,                  
-                  year:'Freshman',
-                  origin:'Brazil',
+                  year:'Sophomore',
+                  origin:'Nepal',
                   srcScreen:'/images/speakers/module/amer.jpg',
                   srcThumb:'/images/speakers/module/amer-thumb.jpg', 
-                  topic:'Ordem e Progresso: Student Voices and the Politics of Sweeping Corruption',
-                  text:'<p>Amer unravels his recent student history by inviting you to have a voice. Having experienced one of the effects of corruption and inequality, Amer shares the story of how he helped organize a student-led movement to fight corruption in Brazil. He offers insights on how students might feel their efforts to make a difference in the world are worthless, and how they believe their immediate actions for progress might not have any long-term effects. He then invites you to critique, how one voice could potentially speak for many, or even how many voices could speak for one. His journey to fight corruption will lead you and him to discover a world of solidarity. The goal of this talk is to demonstrate to students in a larger community that they have a voice; their goal should be to voice themselves louder.</p>'
+                  topic:'Making students lucky',
+                  text:'<p>Krishna made it to an U.S. American university from Nepal. He was lucky, though luck not in a four-leaf clover way, but through various opportunities. In his talk, Krishna describes his new initiative makes other students lucky.</p>'
                   },
-                  {name:'Attilio Rigotti',
+                  {name:'Chaeri Lee',
                   id:2,            
                   year:'Alumni',
-                  origin:'Chile',
+                  origin:'South Korea',
                   srcScreen:'/images/speakers/module/attilio.jpg',
                   srcThumb:'/images/speakers/module/attilio-thumb.jpg',
-                  topic:'How video games saved my life',
-                  text:'<p>Since he can remember, Attilio Rigotti has been obsessed with video games. But it took directing an interactive theater performance to help him figure out why. Simply put, playing video games makes him <i>feel happier</i>. Reflecting on the idea of choice and being a hero in one’s own life, as well as the possibilities of interactivity in the arts, Attilio hopes to better understand what he considers the most important artform of the 21st century and how it can change people\'s lives in the way video games changed his.</p>'
+                  topic:'Where is Waldo really?',
+                  text:'<p>Where is Waldo really? A seemingly simple question on a university application form got Chaeri thinking. What happens if we described ourselves not by what or who we are, but by where we are? In her talk, Chaeri will reflect on self-identity and how we define ourselves.</p>'
                   },
-                  {name:'Charlotte de Bekker',
+                  {name:'Mariam ElZoghbi',
                   id:3,        
                   year:'Sophomore',
-                  origin:'Netherlands',
+                  origin:'Egypt',
                   srcScreen:'/images/speakers/module/charlotte.jpg',
                   srcThumb:'/images/speakers/module/charlotte-thumb.jpg',
-                  topic:'AUTHENT&Iuml;K: IKEA furniture and the construction of authenticity',
-                  text:'<p>In the midst of my hipster angst I was faced with the tragic realisation that my attempts at authenticity were futile. With the help of IKEA furniture, Dutch flea markets, and the image of Dubai, I take a closer look at the irony of constructing authenticity. </p>'
+                  topic:'Revolution of Home',
+                  text:'<p>In January 2011, there were violent revolutions in Egypt. Mariam was trapped in her home, one day making molotov cocktails and the next baking a cake! In her talk, Mariam tells us her story of those revolutions and how Cairo remains as her home. </p>'
                   },
-                  {name:'Hayat Muhammed Seid',               
+                  {name:'Amal Al Gergawi',               
                   id:4,        
                   year:'Sophomore',
-                  origin:'Ethiopia',
+                  origin:'United Arab Emirates',
                   srcScreen:'/images/speakers/module/hayat.jpg',
                   srcThumb:'/images/speakers/module/hayat-thumb.jpg',
-                  topic:'Telling stories and claiming self-identities',
-                  text:'<p>Stories can be a manifestation of our identities; they allow us to express ourselves. Everyone has a story and should have the opportunity to tell it. However, it is the case that some stories are told far more often than others and gradually transform to single narratives and stereotypes. Learning and building common understanding is a gradual and continuous process. We learn through sharing our stories and listening to the stories of others. It is important to continuously provide this space for new stories, new perspectives, and new discussions, which will result in the creation of a more informed, empathetic and respectful society. So the question is, how do we make sure that our voices are heard and that our stories are told the right way?</p>'
+                  topic:'',
+                  text:'<p>As an Emirati, Amal is proud of the traditions and culture in which she lives. Yet sometimes she cannot help feeling a little distant as she thinks about her Egyptian roots through her mother. In her talk, Amal shares her thoughts on her transnationalism and encourages us to embrace the diversity of our heritage. </p>'
                   },
-                  {name:'Jovan Jovancevic',
+                  {name:'Doug Cutchins',
                   id:5,           
-                  year:'Freshman',
-                  origin:'Montenegro',
+                  year:'Staff',
+                  origin:'USA',
                   srcScreen:'/images/speakers/module/jovan.jpg',
                   srcThumb:'/images/speakers/module/jovan-thumb.jpg',
-                  topic:'When vision triumphs over sight',
-                  text:'<p>Training your eyes to see a chessboard that isn’t there and move the game\'s 32 pieces across that very same 64-square matrix; that is the game of blindfold chess. It is a unique challenge that shows what remarkable potential our brains have, a form of training in which everyone can and should engage, regardless of interest in chess. Allow Jovan to patiently guide you through this game, showing you the many ways in which you can benefit from it on a daily basis.</p>'
+                  topic:'How to win a Rhode’s Scholarship',
+                  text:'<p>Doug is a beloved and successful careers advisor at NYUAD. He has an impressive track record of advising two dozen Rhodes national finalists, and four Rhodes scholarship recipients. In his talk, Doug will unveil his key to success and question what success really is!</p>'
                   },
-                  {name:'Maitha Al Memari',
+                  {name:'Jessica Mow',
                   id:6,           
                   year:'Freshman',
-                  origin:'United Arab Emirates',
+                  origin:'USA',
                   srcScreen:'/images/speakers/module/maitha.jpg',
                   srcThumb:'/images/speakers/module/maitha-thumb.jpg',
-                  topic:'De-stereotyping the \'Black Cloak\'',
-                  text:'<p>Why do you wear the same thing everyday? How come you are always in black? Don’t you get bored? What is it? What does it mean? Why do you wear it on a daily basis? How can you express individuality when most Emiratis wear the same thing? My new community at NYUAD bombarded me with absurd questions about my abaya that have never occurred to me before. These questions have made me realize that the way I feel and perceive my abaya is different to how others do. Through my talk I aim to explain and dismiss the stereotype of abayas in the UAE context. Moreover, I aim to motivate people to ask questions and break the stereotypes that surround them with reality.</p>'
+                  topic:'',
+                  text:'<p>After graduating from Tufts University, Jessica came to NYUAD to be a Research Assistant in the Neuroscience of Language Lab. As well as her interest in science, Jessica has a passion for media and has been involved in print and radio media. Science and media have a complicated relationship with the ability to educate or misinform. In her talk, Jessica explores this relationship and suggests ways we could be more effective in our efforts.</p>'
                   },
-                  {name:'Meera Al Agroobi',
+                  {name:'Yahya John Scaccia',
                   id:7,           
-                  year:'Graduate',                               
-                  origin:'United Arab Emirates',                    
+                  year:'Sophomore',                               
+                  origin:'USA',                    
                   srcScreen:'/images/speakers/module/meera.jpg',      
                   srcThumb:'/images/speakers/module/meera-thumb.jpg',
-                  topic:'Bullies are victims too!',
-                  text:'<p>Should bullies be punished or rewarded? It seems that there is an obvious answer to this question but you’d be surprised to know that there isn’t. I am going to share my story as a bully and introduce new ideas and interventions for bullies. You can’t fix a car unless you know how the mechanics work, right? Similarly you can’t fix bullies unless you know how a bully’s minds work. I was once a bully and I can tell you exactly how it works.</p>'
+                  topic:'My Jihad',
+                  text:'<p>Jihad in Arabic means an act of struggle. On receiving his offer to attend New York University Abu Dhabi, Yahya received a less-than-supportive letter from his grandmother. She said ”Your decision that you make now will affect you for the rest of your life” and she was right. In his talk, Yahya addresses the letter and discusses his jihad is to raise awareness of the true values and beauty of Islam.</p>'
                   },
-                  {name:'Mohit Mandal',
+                  {name:'Joanna Bui',
                   id:8,
                   year:'Senior',
-                  origin:'India',
+                  origin:'Australia',
                   srcScreen:'/images/speakers/module/mohit.jpg',
                   srcThumb:'/images/speakers/module/mohit-thumb.jpg',
-                  topic:'What cricket taught me about compassion',
-                  text:'<p>A research assignment and a chance encounter in a barbershop provided me with the spark for a fourteen-week long journey into the hidden world of Abu Dhabi\'s "migrant workers." From 12am to 3am on weekend nights, I played street-cricket with a group of Indian, Pakistani and Bangladeshi hairdressers in the abandoned parking lot of a mall in Abu Dhabi and found myself confronted, continually, with the conundrum: How does one begin to capture the rich, complex - and, often, invisible - lives of the "migrant workers" in a way that is neither condescending nor patronizing?</p>'    
-                  },
-                  {name:'Samuel Ridgeway',
-                  id:10,            
-                  year:'Junior',
-                  origin:'United Kingdom',
-                  srcScreen:'/images/speakers/module/samuel.jpg',
-                  srcThumb:'/images/speakers/module/samuel-thumb.jpg',
-                  topic:'Let me take you home...',
-                  text:'<p>Who is home for you? What is home for you? Where is home for you? I used to have only one home – it was the place where I grew up, lived with my family, and I felt very comfortable there. Fast-forward through three years of college in Abu Dhabi and I now find myself questioning what home is and how to find it. I want to explore the idea that a person can have multiple homes, ways a place can become home and the importance of developing memories where you are in life.</p>'
-                  },
-                  {name:'Sara Al Shamlan',
-                  id:11,            
-                  year:'Senior',
-                  origin:'Qatar',
-                  srcScreen:'/images/speakers/module/sara.jpg',
-                  srcThumb:'/images/speakers/module/sara-thumb.jpg',
-                  topic:'Curiosity Saved the Cat',
-                  text:'<p>How the words of my mother led to a food donation drive, how the tug of an abaya became an educational support program, how a game of football in Mozambique created an overseas call to action, how the desire for a family portrait built a roof and a home. This is a story of how curiosity and unlikely interactions with those different from myself led me to reevaluate my life and find a passion worth pursuing.</p>'
-                  },                              
-                  {name:'Vasily Rudchenko',
-                  id:12,           
-                  year:'Sophomore',
-                  origin:'Ukraine',
-                  srcScreen:'/images/speakers/module/vasily.jpg',                   
-                  srcThumb:'/images/speakers/module/vasily-thumb.jpg',
-                  topic:'What happens when engineers stop caring about technology?',
-                  text:'<p>Engineers are known for creating cool technology without a problem that would justify its existence. I am working on a team of NYUAD engineers, who are solving an existing problem in a national park in the UAE by tackling it through a human-centered approach, rather than a strictly technological mindset.</p>'
-                }];
+                  topic:'Linear Kharma',
+                  text:'<p>What goes around comes around? That’s what they say, but what if in your decisions today include thoughts of the past and future and what you do today goes on to affect someone else? Joey’s parents fled the Vietnam War and arrived in Australia as refugees. Today she has the immigrant drive to do well not only for herself, but also for her parents and her grandparents. In her talk, Joey will present the idea of linear kharma in which what you do today can inspire and impact others for the greater good.</p>'    
+                  }                
+                ];
 
 
 exports = module.exports = {
@@ -113,7 +87,7 @@ exports = module.exports = {
                                   { template:'standard',
                                     macro:'standard',
                                     arguments:{title:'Speaker Selection and Training',
-                                               text:'<p>What\'s your story was the simple question posed to TEDxNYUAD applicants in the selection process that started in October 2014. Shortening the original list of 60 applicants to just 28, the remainder pitched their speeches to the organizing team, who had the difficult task of narrowing down these extraordinary stories to those featured in this year’s TEDx event.</p>'
+                                               text:'<p>Represent You was the simple statement posed to TEDxNYUAD applicants in the selection process that started in October 2015. Shortening the original list of 60 applicants to just 30, the remainder pitched their speeches to the organizing team, who had the difficult task of narrowing down these extraordinary stories to those featured in this year’s TEDx event.</p>'
                                               }
                                   }],
                         speakers: speakersArray};
